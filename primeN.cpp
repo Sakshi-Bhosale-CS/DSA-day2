206fe4542180d58ac3304db38165e3cbe21738e5
Sakshi-Bhosale-CS <bhosalesakshi906@gmail.com>
2025-08-29 20:17:45 +0530

    All files of day 2 added

---
+++ primeN.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    bool isprime = true;
+
+    if(n <= 2)
+    {
+        cout<<n<<" is a invalid number.";
+    }
+    for(int i = 2 ; i < n ; i++)
+    {
+        if(n % i == 0)
+        {
+            isprime = false;
+            break;
+        }
+
+    }
+    
+    if(isprime)
+        cout<<n<<" is a prime number";
+    else
+        cout<<n<<" is not a prime number";
+
+    return 0;
+}
